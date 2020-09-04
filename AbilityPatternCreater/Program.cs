8f2d4d1e8bccbb91fa756c65601be79bcc4ba90c
with-leaf-tech <with_leaf@hotmail.co.jp>
2020-09-04 21:07:31 +0900

    アビリティパターン登録

---
+++ AbilityPatternCreater/Program.cs
@@ -0,0 +1,19 @@
+﻿using ItemClassLibrary.Entity;
+using ItemClassLibrary.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbilityPatternCreater {
+    class Program {
+        static void Main(string[] args) {
+            AbilityPattern app = new AbilityPattern(Utility.PARTS_HEAD, "",
+                new List<string[]> {
+                    new string[]{ "テスト1+(num)", "3,5,7", "0,1,2,3,-2|0,2,3,4,-3|0,3,5,-4" },
+                    new string[]{ "テスト2+(num)", "5,7,10", "4,-3|2,5,-4|4,7,-5" },
+                }, "%");
+        }
+    }
+}
---
+++ ItemClassLibrary/Entity/AbilityPattern.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemClassLibrary.Entity {
+    public class AbilityPattern {
+        private string replaceString = "(num)";
+        public string Classification { get; set; }
+        public string Name { get; set; }
+        public List<string> PatternList { get; set; }
+        public AbilityPattern(string classification, string name, List<string[]> param, string option) {
+            this.Classification = classification;
+            this.Name = name;
+            PatternList = new List<string>();
+            for (int i = 0; i < param.Count; i++) {
+                string baseString = param[i][0];
+                string[] baseParams = param[i][1].Replace(" ", "").Split(new char[] { ',' });
+                for(int j = 0; j < baseParams.Length; j++) {
+                    if(param[i].Length > 2) {
+                        string[] paramList = param[i][2].Replace(" ", "").Split(new char[] { '|' });
+                        string[] status = paramList[j].Replace(" ", "").Split(new char[] { ',' });
+                        for (int k = 0; k < status.Length; k++) {
+                            string appendNum = status[k] == "0" ? "" : float.Parse(status[k]) > 0 ? "(+" + status[k] + ")" : "(" + status[k] + ")";
+                            PatternList.Add(baseString.Replace(replaceString, baseParams[j].ToString() + appendNum + option));
+
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
